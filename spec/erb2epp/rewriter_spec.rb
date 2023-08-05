227c7348534c686d2bafc6117e0df0f1e1977abc
Yury Bushmelev <jay4mail@gmail.com>
2023-08-05 23:04:31 +0400

    Add more unit tests

--- spec/erb2epp/rewriter_spec.rb
+++ spec/erb2epp/rewriter_spec.rb
@@ -6,22 +6,83 @@ describe Erb2epp::Rewriter do
   describe '.rewrite_code' do
     subject(:rewritten) { described_class.new.rewrite_code(code) }
 
-    context 'with empty string' do
-      let(:code) { '' }
+    [
+      {
+        code: '',
+        match: '',
+      },
+      {
+        code: '@x',
+        match: '$x',
+      },
+      {
+        code: 'var_x = true',
+        match: '$var_x = true',
+      },
+      {
+        code: '(var1, var2) = var_arr',
+        match: '($var1, $var2) = var_arr',
+      },
+      {
+        code: 'if @x',
+        match: 'if $x {',
+      },
+      {
+        code: 'if @x && @y',
+        match: 'if $x and $y {',
+      },
+      {
+        code: 'if @x || @y',
+        match: 'if $x or $y {',
+      },
+      {
+        code: 'if @x then',
+        match: 'if $x {',
+      },
+      {
+        code: 'if @x {',
+        match: 'if $x {',
+      },
+      {
+        code: '@x.each do |k,v|',
+        match: '$x.each |$k,$v| {',
+      },
+      {
+        code: '@x.each {|k,v|',
+        match: '$x.each |$k,$v| {',
+      },
+      {
+        code: 'else',
+        match: '} else {',
+      },
+      {
+        code: 'end',
+        match: '}',
+      },
+      # Ensure ERB comments are retained
+      {
+        code: '# the comment #',
+        match: '# the comment #',
+      },
+      # Ensure whitespace control is retained
+      {
+        code: '- trim_left',
+        match: '- trim_left',
+      },
+      {
+        code: 'trim_right -',
+        match: 'trim_right -',
+      },
+      {
+        code: '- trim_both -',
+        match: '- trim_both -',
+      },
+    ].each do |param|
+      context "with code => '#{param[:code]}'" do
+        let(:code) { param[:code] }
 
-      it { is_expected.to eq('') }
-    end
-
-    context 'with an instance variable' do
-      let(:code) { '@x' }
-
-      it { is_expected.to eq('$x') }
-    end
-
-    context 'with simple if' do
-      let(:code) { 'if @x' }
-
-      it { is_expected.to eq('if $x {') }
+        it { is_expected.to eq(param[:match]) }
+      end
     end
   end
 end
